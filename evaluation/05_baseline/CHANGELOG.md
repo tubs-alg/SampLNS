c5feec21354185606d4f7d6ebf1055e4337ab291
Dominik Krupke <krupke@ibr.cs.tu-bs.de>
2023-08-17 16:55:17 +0200

    some clean up in the data

---
+++ evaluation/05_baseline/CHANGELOG.md
@@ -0,0 +1,1 @@
+* *2023-08-17 (DK):* Noted that the results only contain a subset of the benchmark instances. The comparison, thus, is flawed.
--- evaluation/06_cds_improvements/_conf.py
+++ evaluation/06_cds_improvements/_conf.py
@@ -1,3 +1,5 @@
+# All general properties and dependencies of this experiment and evaluation should be defined here.
+
 ITERATIONS = 10000
 ITERATION_TIME_LIMIT = 60.0
 TIME_LIMIT = 900
@@ -6,3 +8,5 @@ BASE = "900_seconds_5_it"
 RESULT_FOLDER = f"01_results/{BASE}_{TIME_LIMIT}"
 INPUT_SAMPLE_ARCHIVE = f"../01_ICSE_2024_0/00_baseline/{BASE}.zip"
 INSTANCE_ARCHIVE = "../../benchmark_models.zip"
+EXTENDED_BASELINE_DATA = "../05_baseline/03_baseline_gap_data.json.zip"
+EXTERNAL_BOUNDS = ["../07_long_optimization/out/06_best_bounds.csv", "../08_longer_iterations/out/06_best_bounds.csv"]
