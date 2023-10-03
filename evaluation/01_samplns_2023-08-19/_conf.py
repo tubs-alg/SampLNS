92e68d5c99432376825c1cd578d89b0a3a399b40
Dominik Krupke <krupke@ibr.cs.tu-bs.de>
2023-10-03 19:39:44 +0200

    updates to the evaluation for the comments of thomas

--- evaluation/01_samplns_2023-08-19/_conf.py
+++ evaluation/01_samplns_2023-08-19/_conf.py
@@ -15,7 +15,7 @@ EXTENDED_BASELINE_DATA = "../00_baseline/OUTPUT/01_simple_baseline_data.json.zip
 EXTERNAL_BOUNDS = [
   #  "../02_60min_samplns_2023-08-19/out/06_best_bounds.csv",
   #  "../03_60min_samplns_long_iterations_2023-08-19/out/06_best_bounds.csv",
-    "../04_super_long_samplns/out/06_best_bounds.csv",
+    "../04_super_long_samplns/OUTPUT/06_best_bounds.csv",
 ]
 PREPROCESSED_DATA = "./OUTPUT/04_preprocessed_data.json.zip"
 LOWER_BOUNDS = "./OUTPUT/04_lower_bounds.json.zip"
